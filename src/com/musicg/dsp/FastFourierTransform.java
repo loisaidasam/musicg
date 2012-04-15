3e176dedbe092d2fc0791a2c4d51b6aeedca4fa0
Jacquet Wong <jacquetwong@gmail.com>
2012-04-15 20:34:00 +0800

    Extract positive frequency only in FFT magnitude

--- src/com/musicg/dsp/FastFourierTransform.java
+++ src/com/musicg/dsp/FastFourierTransform.java
@@ -42,14 +42,20 @@ public class FastFourierTransform {
 		// end call the fft and transform the complex numbers
 		
 		double[] complexNumbers=amplitudes;
+
 		// even indexes (0,2,4,6,...) are real parts
 		// odd indexes (1,3,5,7,...) are img parts
-
-		// FFT produces a transformed pair of arrays where the first half of the values represent positive frequency components and the second half represents negative frequency components.
-		double[] mag = new double[sampleSize/2];
-		for (int i = 0; i < sampleSize; i+=2)
-			mag[i/2] = Math.sqrt(complexNumbers[i] * complexNumbers[i]+ complexNumbers[i+1] * complexNumbers[i+1]);
+		int indexSize=sampleSize/2;
 		
+		// FFT produces a transformed pair of arrays where the first half of the values represent positive frequency components and the second half represents negative frequency components.
+		// we omit the negative ones
+		int positiveSize=indexSize/2;
+		
+		double[] mag = new double[positiveSize];
+		for (int i = 0; i < indexSize; i+=2){
+			mag[i/2] = Math.sqrt(complexNumbers[i] * complexNumbers[i]+ complexNumbers[i+1] * complexNumbers[i+1]);
+		}
+
 		return mag;
 	}
 	
